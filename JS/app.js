b4e10c30e12fc798b3444f7f135ef24ee6cff5d8
Nazmul Ahsan <104188506+Ahsan-xeeshan@users.noreply.github.com>
2022-08-11 23:53:49 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,81 @@
+
+var typed = new Typed('#orderText', {
+    strings: [ 'Order Now!','Get your product quickly','Order Now!', 'Stock Limited'],
+    typeSpeed: 70,  
+    backdelay: 70,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|',
+    smartBackspace: true// Default value
+  });
+
+
+
+var mixer = mixitup('.filterableItems');
+
+
+$('#saveMe').click(function(){
+    Swal.fire({
+        title: 'Do you want to save the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire('Saved!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+})
+
+$('#deleteMe').click(function(){
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        }
+      })
+})
+
+
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+			counterUp( el, {
+				duration: 2000,
+				delay: 16,
+			} )
+			el.classList.add( 'is-visible' )
+		}
+	} )
+}
+
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
+
+const el = document.querySelector( '.counter' )
+IO.observe( el )
+
+
+function addDarkmodeWidget() {
+    new Darkmode({
+        label: '🌓',
+    }).showWidget();
+  }
+  window.addEventListener('load', addDarkmodeWidget);
